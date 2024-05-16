eb17a55a6b564a186ea72f9dcb7e28dc19b83f33
mtakk <forsyuka321@gmail.com>
2024-05-16 13:55:27 +0900

    ヘッダー追加(最小の変更が反映されることを確認)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react"
+
+export const Header: FC = () => {
+    return (
+        <header>ヘッダーです</header>
+    )
+}
